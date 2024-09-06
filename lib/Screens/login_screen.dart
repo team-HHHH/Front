c2a9b28551e8d6fc4447910ceadb53a193cdd7e7
LEE <ljhlmkljhlmkljhlmk@naver.com>
2024-09-06 17:34:05 +0900

    로그인 페이지 UI 구성

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>(); // Form 위젯을 위해 사용.
+
+  String _enteredId = "";
+  String _enteredPassword = "";
+
+  // 로그인 버튼 누를 시 수행.
+  void handleLogin() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0), // 구분선의 두께
+          child: Divider(
+            thickness: 1.0,
+            height: 1.0,
+            color: Colors.grey.shade300, // 구분선 색상
+          ),
+        ),
+        title: const Text(
+          "로그인",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 60),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "귀찮은 포스터 요약은",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    "혁혁호형",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                    child: TextFormField(
+                      onSaved: (newValue) {
+                        _enteredId = newValue!;
+                      },
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "아이디",
+                        contentPadding: const EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: const BorderSide(
+                              color: Colors.grey), // 비활성화 상태의 테두리 색상
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: const BorderSide(
+                                color: Colors.grey) // 활성화 상태의 테두리 색상
+                            ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor,
+                            width: 2.0,
+                          ), // 포커스 상태에서 테두리 색상
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  SizedBox(
+                    height: 40,
+                    child: TextFormField(
+                      onSaved: (newValue) {
+                        _enteredPassword = newValue!;
+                      },
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "비밀번호",
+                        contentPadding: const EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: const BorderSide(
+                              color: Colors.grey), // 비활성화 상태의 테두리 색상
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: const BorderSide(
+                                color: Colors.grey) // 활성화 상태의 테두리 색상
+                            ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(
+                            color: Theme.of(context).primaryColor,
+                            width: 2.0,
+                          ), // 포커스 상태에서 테두리 색상
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 40,
+                    child: TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.save();
+                      },
+                      style: TextButton.styleFrom(
+                        splashFactory: NoSplash.splashFactory,
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(0),
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "회원가입",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(0),
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  child: const Text(
+                    "아이디 찾기",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  child: const Text(
+                    "비밀번호 찾기",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
