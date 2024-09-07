a2c07e636a996c14dc7e079a28528277a96e97ba
leejoonhyeock <ljhlmkljhlmkljhlmk@naver.com>
2024-09-07 20:15:19 +0900

    AndroidManifest.xml 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_story.dart';
 import 'package:scheduler/Screens/login_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(
+    nativeAppKey: '1e183dbd3fe27b83dd4f6c01898a258f',
+  );
   runApp(const MyApp());
 }
 
